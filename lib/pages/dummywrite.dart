eef745afcfd269f7d4eafef91d6a8b3eaa873cd9
sumitpant190 <114337820+sumitpant190@users.noreply.github.com>
2023-01-12 19:38:47 +0545

    resized screen

---
+++ lib/pages/dummywrite.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class MyWidget extends StatefulWidget {
+  const MyWidget({super.key});
+
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('hello'),
+      ),
+    );
+  }
+}
