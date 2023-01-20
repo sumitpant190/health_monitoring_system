482032ff464d1503812029315e8465f3b29f3913
sumitpant190 <114337820+sumitpant190@users.noreply.github.com>
2023-01-20 10:06:27 +0545

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:health_monitoring_system/pages/homepage.dart';
-import 'package:health_monitoring_system/pages/notifications_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -13,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: NotificationPage(),
+      home: HomePage(),
     );
   }
 }
--- lib/pages/logs.dart
+++ lib/pages/logs.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 class LogsWidget extends StatefulWidget {
   const LogsWidget({super.key});
